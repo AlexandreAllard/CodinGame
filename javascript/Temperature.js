75e23187e7a361126e93b99196144bcb0b18952e
Alexandre <alrdalexandre@gmail.com>
2022-04-08 22:17:57 +0200

    Temperature exercise correction

--- javascript/Temperature.js
+++ javascript/Temperature.js
@@ -3,7 +3,9 @@ let lowerTemp;
 var inputs = readline().split(' ');
 for (let i = 0; i < n; i++) {
     const temp = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+    if (temp!=lowerTemp){
         (Math.abs(temp)==Math.abs(lowerTemp)) ? lowerTemp=Math.abs(temp) : (Math.abs(temp)>=Math.abs(lowerTemp)) ? lowerTemp=lowerTemp : lowerTemp=temp;
+    }
 }
 
 if (n==0){
